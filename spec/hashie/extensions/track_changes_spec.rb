9b446e8bc99cfb33973ea03384305c4ba71eb193
Steven Davidovitz <sdavidovitz@zendesk.com>
2012-06-22 16:10:58 -0700

    use anonymous subclass to test TrackChanges

--- spec/hashie/extensions/track_changes_spec.rb
+++ spec/hashie/extensions/track_changes_spec.rb
@@ -1,11 +1,13 @@
 require 'spec_helper'
 
-
 describe Hashie::Extensions::TrackChanges do
-  before(:each) do
-    class ExampleTrackableHash < Hash; include Hashie::Extensions::TrackChanges end
-  end
-  subject { ExampleTrackableHash.new }
+  let(:subclass) {
+    Class.new(Hash) do
+    include Hashie::Extensions::TrackChanges
+    end
+  }
+
+  subject { subclass.new }
 
   context "single level hash" do
     before(:each) do
@@ -20,19 +22,15 @@ describe Hashie::Extensions::TrackChanges do
       subject.changes[:key].should == subject[:key]
     end
   end
-  
+
   context "multi-level hash" do
     before(:each) do
-      subject[:key] = ExampleTrackableHash.new
+      subject[:key] = subclass.new
       subject[:key][:foo] = true
     end
 
     specify "changes should be recursive" do
       subject.changes[:key][:foo].should == subject[:key][:foo]
     end
-  end
-
-  after(:each) do
-    Object.send(:remove_const, :ExampleTrackableHash)
   end
 end
